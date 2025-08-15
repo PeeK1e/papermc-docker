fd24ae22cd9d2c9b979e7d0942aba27ce832076a
Ryu <me@peek1e.eu>
2025-08-15 16:22:38 +0200

    update: image builder

---
+++ build/tools.Containerfile
@@ -0,0 +1,4 @@
+FROM docker.io/library/alpine:3.22
+
+# hadolint ignore=DL3018
+RUN apk add --no-cache jq
---
+++ getpaper.sh
@@ -0,0 +1,16 @@
+#/usr/bin/env bash
+
+set -euxo pipefail
+
+PAPER_VERSION="$(curl --silent -X 'GET' 'https://fill.papermc.io/v3/projects/paper/versions' -H 'accept: application/json' | jq '.versions[0].version.id' -r)"
+
+PAPER_LATEST_INFO="$(curl --silent -X 'GET' -H 'accept: applications/json' https://fill.papermc.io/v3/projects/paper/versions/${PAPER_VERSION}/builds/latest)"
+
+PAPER_BUILD_ID="$(echo ${PAPER_LATEST_INFO} | jq '.id' -r)"
+PAPER_BUILD_SHA="$(echo ${PAPER_LATEST_INFO} | jq '.downloads."server:default".checksums.sha256' -r)"
+PAPER_BUILD_URL="$(echo ${PAPER_LATEST_INFO} | jq '.downloads."server:default".url' -r)"
+
+CONTAINER_TAG="${PAPER_VERSION}" #-${PAPER_BUILD_ID}"
+
+echo "PAPER_URL=${PAPER_BUILD_URL}" >> .dotenv
+echo "IMAGE_TAG=${CONTAINER_TAG}" >> .dotenv
